d88c28afb379de0adc8dfc899361eb50f6fa3596
Amy Westcott <amy.westcott@yahoo.com>
2019-08-08 23:20:52 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -3,7 +3,7 @@ class Puppy
   attr_accessor :name, :months_old
   attr_reader :breed
   
-  def initialize(name, months_old, :breed)
+  def initialize(name, months_old, breed:)
     @name = name 
     @months_old = months_old
     @breed 
